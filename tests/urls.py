87618175afed7ebf343f03b6841fb80931788708
Joshua Massover <joshua.massover@gmail.com>
2017-08-20 13:26:33 -0400

    Move tests

--- tests/urls.py
+++ tests/urls.py
@@ -3,8 +3,4 @@ from __future__ import unicode_literals, absolute_import
 
 from django.conf.urls import url, include
 
-from bootstrap_select.urls import urlpatterns as bootstrap_select_urls
-
-urlpatterns = [
-    url(r'^', include(bootstrap_select_urls, namespace='bootstrap_select')),
-]
+urlpatterns = []
